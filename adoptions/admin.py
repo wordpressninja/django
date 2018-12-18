57f791a1ee5c48d492b3ee09a15e6d693efe63e0
Chris Foster <wordpressninja@me.com>
2018-12-18 03:22:07 +0000

    removing file

--- adoptions/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Pet
-
-# Add the admin model for app
-@admin.register(Pet)
-class PetAdmin(admin.ModelAdmin):
-    list_display = ['name', 'species', 'breed', 'age', 'sex']
--- adoptions/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AdoptionsConfig(AppConfig):
-    name = 'adoptions'
